ad672f15d3980673f666e700b794838824cd2efc
Britt Crawford <britt.v.crawford@gmail.com>
2016-03-22 16:54:03 -0700

    Moving all kinds of stuff

--- html.js
+++ html.js
@@ -1,45 +1,46 @@
 import React from 'react'
 import DocumentTitle from 'react-document-title'
-
 import { link } from 'gatsby-helpers'
 import { TypographyStyle } from 'utils/typography'
+// import GoolgleAnalytics from 'utils/google_analytics'
 
-module.exports = React.createClass({
-  propTypes () {
-    return {
-      title: React.PropTypes.string,
-    }
-  },
-  render () {
-    let title = DocumentTitle.rewind()
-    if (this.props.title) {
-      title = this.props.title
-    }
+module.exports = ({title, favicon, body}) => {
+  let docTitle = DocumentTitle.rewind()
+  if (title) {
+    docTitle = title
+  }
 
-    let cssLink
-    if (process.env.NODE_ENV === 'production') {
-      cssLink = <link rel="stylesheet" href={link('/styles.css')} />
-    }
+  let cssLink
+  if (process.env.NODE_ENV === 'production') {
+    cssLink = <link rel="stylesheet" href={link('/styles.css')} />
+  }
 
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0 maximum-scale=1.0"
-          />
-          <title>{title}</title>
-          <link rel="shortcut icon" href={this.props.favicon} />
-          <TypographyStyle />
-          {cssLink}
-        </head>
-        <body>
-          <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
-          <script src={link('/bundle.js')} />
-        </body>
-      </html>
-    )
-  },
-})
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport"
+          content="width=device-width, initial-scale=1.0 maximum-scale=1.0" />
+        <title>{docTitle}</title>
+
+        <meta name="description" content="Britt Crawford is just a guy who lives in San Francisco with his family." />
+        <meta name="keywords" content="programming, data, analysis, food, cocktails, dilettante, adventurer, gentleman" />
+        <meta name="author" content="Britt Crawford" />
+
+        <link rel="shortcut icon" href={favicon} />
+        <link rel="apple-touch-icon" href="/assets/img/apple-touch-icon-57-precomposed.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/img/apple-touch-icon-72-precomposed.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/img/apple-touch-icon-114-precomposed.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/img/apple-touch-icon-144-precomposed.png" />
+        
+        {cssLink}
+      </head>
+      <body>
+        <div id="react-mount" dangerouslySetInnerHTML={{ __html: body }} />
+        <script src={link('/bundle.js')} />
+      </body>
+    </html>
+  )
+}
+
--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,56 +1,46 @@
 import React from 'react'
-import { Container } from 'react-responsive-grid'
 import { Link } from 'react-router'
 import { link } from 'gatsby-helpers'
 import Headroom from 'react-headroom'
 
-import { rhythm } from 'utils/typography'
-
-module.exports = React.createClass({
-  propTypes () {
-    return {
-      children: React.PropTypes.any,
-    }
-  },
-  render () {
-    return (
-      <div>
-        <Headroom
-          wrapperStyle={{
-            marginBottom: rhythm(1),
-          }}
-          style={{
-            background: 'lightgray',
-          }}
-        >
-          <Container
-            style={{
-              maxWidth: 960,
-              paddingTop: 0,
-              padding: `${rhythm(1)} ${rhythm(1/2)}`,
-            }}
-          >
-            <Link
-              to={link('/')}
-              style={{
-                color: 'black',
-                textDecoration: 'none',
-              }}
-            >
-              Gatsby!!!
-            </Link>
-          </Container>
-        </Headroom>
+module.exports = ({children}) => {
+  return (
+    <div>
+      <Headroom
+        wrapperStyle={{
+          marginBottom: rhythm(1),
+        }}
+        style={{
+          background: 'lightgray',
+        }}
+      >
         <Container
           style={{
             maxWidth: 960,
-            padding: `${rhythm(1)} ${rhythm(1/2)}`,
             paddingTop: 0,
+            padding: `${rhythm(1)} ${rhythm(1/2)}`,
           }}
         >
-          {this.props.children}
+          <Link
+            to={link('/')}
+            style={{
+              color: 'black',
+              textDecoration: 'none',
+            }}
+          >
+            Gatsby!!!
+          </Link>
         </Container>
-      </div>
-    )
-  },
-})
+      </Headroom>
+      <Container
+        style={{
+          maxWidth: 960,
+          padding: `${rhythm(1)} ${rhythm(1/2)}`,
+          paddingTop: 0,
+        }}
+      >
+        {children}
+      </Container>
+    </div>
+  )
+}
