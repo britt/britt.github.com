89de127982ec2c58c05d1601d1067682d0f885aa
Britt Crawford <britt.v.crawford@gmail.com>
2016-03-23 11:14:25 -0700

    Add some media queries

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default ({route}) => {
               <a href="https://www.facebook.com/britt.crawford" title="My Facebook Profile" rel="me"><i className="fa fa-facebook fa-fw"></i><span>https://www.facebook.com/britt.crawford</span></a>
             </li>
             <li>
-              <a href="http://www.linkedin.com/in/brittcrawford/" title="My LinkedIn Profile" rel="me"><i className="fa fa-linkedin fa-fw"></i><span>http://www.linkedin.com/in/brittcrawford/</span></a>
+              <a href="http://www.linkedin.com/in/brittcrawford/" title="My LinkedIn Profile" rel="me"><i className="fa fa-linkedin fa-fw"></i><span>https://www.linkedin.com/in/brittcrawford/</span></a>
             </li> 
             <li>
               <a href="https://github.com/britt" title="GitHub" rel="me"><i className="fa fa-github-alt fa-fw"></i><span>https://github.com/britt</span></a>
@@ -42,8 +42,8 @@ export default ({route}) => {
               <a href="bitcoin:17nHbStzmWgcBEBSgVFT94Eo5hoq9h7Hh1" title="Bitcoin"><i className="fa fa-bitcoin fa-fw"></i><span>Bitcoin</span></a>
             </li> 
             <li>
-              <a href="/britt.gpg" title="PGP/GPG Public Key"><i className="fa fa-lock fa-fw"></i><span>PGP/GPG Key</span></a> ::: 
-              <a href="https://keybase.io/britt" title="Keybase"><span>Keybase</span></a> ::: 
+              <a href="/britt.gpg" title="PGP/GPG Public Key"><i className="fa fa-lock fa-fw"></i><span>PGP/GPG Key</span></a> <span>:::</span> 
+              <a href="https://keybase.io/britt" title="Keybase"><span>Keybase</span></a> <span>:::</span> 
               <a href="http://pgp.mit.edu/pks/lookup?op=vindex&search=0x7C887E4EE58C84B3" title="MIT PGP Public Key Server"><span>MIT</span></a>
             </li>                                                       
           </ul>
