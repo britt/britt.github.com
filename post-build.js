be6feccaec78de72ac4e21487c7db6dcd66deea9
Britt Crawford <britt.v.crawford@gmail.com>
2016-04-11 22:23:53 -0700

    Got Feed generation owrking without content

--- post-build.js
+++ post-build.js
@@ -1,10 +1,11 @@
+import FS from 'fs'
 import Shell from 'child_process'
 import Feed from 'feed'
-import moment from 'moment'
-import MarkdownIt from 'markdown-it'
-import FrontMatter from 'front-matter'
-import FS from 'fs'
 import TOML from 'toml-js'
+import PageUtils from './lib/page_utils'
+import { Pages } from './lib/sort_utils'
+import moment from 'moment'
+import frontmatter from 'front-matter'
 
 function thereIsNoTry(callback) {
   return (err, data) => {
@@ -17,70 +18,67 @@ function thereIsNoTry(callback) {
   }
 }
 
-function copyAssets() {
+function generateFeed(pages) {
+  FS.readFile('config.toml', thereIsNoTry((data) => {
+    const config = TOML.parse(data)
+    const now = moment()
+    const feed = new Feed({
+      title:       config.siteTitle,
+      description: config.description,
+      link:        config.link,
+      id: `tag:${config.siteTitle},${now.format('YYYY-mm-DD')}:/${now.format('YYYYmmDDX')}`,
+      copyright:   `All rights reserved 2016, ${config.author.fullName}`,    
+      author: {
+          name:    config.author.fullName,
+          email:   config.author.email,
+          link:    config.link
+      }
+    })
+
+    feed.addContributor({
+      name: config.author.fullName,
+      email: config.author.email,
+      link: config.link
+    })
+
+    let feedPages = pages.filter((page) => PageUtils.isDatedPost(page.path))
+                      .sort(Pages.dateInPath)
+                      .reverse()
+                      .slice(0,10)
+
+    for(let feedPage of feedPages) {
+      const metaData = {
+        date: PageUtils.extractDatePart(feedPage.path)
+      }
+
+      const pageData = FS.readFileSync(
+        `${__dirname}/pages/${feedPage.requirePath}`,
+        'utf-8')
+
+      if(feedPage['file']['ext'] == 'md') {
+        metaData.title = frontmatter(pageData).attributes.title
+      } else if(feedPage['file']['ext'] == 'json'){
+        metaData.title = JSON.parse(pageData)['title']
+      }
+
+      feed.addItem({
+        title: metaData.title,
+        link: `${config.link}${feedPage.path}`,
+        content: "<div>CONTENT</div>",
+        id: `tag:${config.siteTitle},${metaData.date}:${feedPage.path}`,
+        date: moment(metaData.date).toDate()
+      })
+    }
+
+    FS.writeFileSync(`${__dirname}/public/feed.xml`, feed.render('atom-1.0'))
+  }))
+}
+
+export default (pages, callback) => {
   Shell.execSync("cp -r assets/* public/")
   Shell.execSync("cp -r assets/.nojekyll public/")
   Shell.execSync("cp -r assets/.gitignore public/")
   Shell.execSync("cp -r assets/.well-known public/")
-}
-
-function addItem(feed, page) {
-  feed.addItem({
-    title: page.data.title
-    link: "http://brittcrawford.com#{page.path}"
-    date: moment(page.data.date).toDate()
-    content: md.render(
-      frontmatter(
-        fs.readFileSync(
-          "#{__dirname}/pages/#{page.requirePath}",
-          'utf-8'
-        )
-      ).body
-    )
-    author: [{
-      name: "Britt Crawford"
-      email: "britt@illtemper.org"
-      link: "http://brittcrawford.com"
-    }]
-  })
-}
-
-function generateAtomFeed(pages, configData) {
-  const config = TOML.parse(configData)
-
-  md = MarkdownIt({
-    html: true
-    linkify: true
-    typographer: true
-  })
-
-  feed = new Feed({
-    title:       config.siteTitle,
-    description: config.description,
-    link:        'http://brittcrawford.com/',
-    copyright:   'All rights reserved 2016, Britt Crawford',
-    author: {
-      name:    'Britt Crawford',
-      email:   'britt@illtemper.org',
-    }
-  })
-
-  feed.addContributor({
-    name: 'Britt Crawford'
-    email: 'britt@illtemper.org'
-    link: 'http://brittcrawford.com'
-  })
-
-  const feedPages = pages.filter((page) => {
-    const datePart = Page.extractDateFromPath(page.path)
-    return datePart != undefined && datePart != null
-  })
-
-  feedPages.sort(Pages.byDate).reverse().slice(0,10).map(addItem)
-  fs.writeFileSync "#{__dirname}/public/atom.xml", feed.render('atom-1.0')
-}
-
-export default (pages, callback) => {
-  // FS.readFile('config.toml', thereIsNoTry(generateAtomFeed.bind(this, pages)))
-  copyAssets()
+  generateFeed(pages)
+  callback()
 }
