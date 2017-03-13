f698edd08464ff58a66d4c407df60a72a670c8e2
Britt Crawford <britt.v.crawford@gmail.com>
2017-03-13 05:15:01 -0400

    Publish site - automated

--- public/render-page.js
+++ public/render-page.js
@@ -29987,6 +29987,25 @@ return /******/ (function(modules) { // webpackBootstrap
 			{
 				"file": {
 					"root": "",
+					"dir": "reading/2017-03-12",
+					"base": "index.json",
+					"ext": "json",
+					"name": "index",
+					"extname": ".json",
+					"basename": "index.json",
+					"dirname": "reading/2017-03-12",
+					"stem": "index",
+					"path": "reading/2017-03-12/index.json",
+					"absolute": "/apps/britt.github.com/reading/2017-03-12/index.json",
+					"isAbsolute": false
+				},
+				"requirePath": "reading/2017-03-12/index.json",
+				"path": "/reading/2017-03-12/",
+				"data": {}
+			},
+			{
+				"file": {
+					"root": "",
 					"dir": "reading",
 					"base": "index.jsx",
 					"ext": "jsx",
