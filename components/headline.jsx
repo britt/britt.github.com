0c42e859d88ca20837850f01712f019be07c431f
Britt Crawford <britt.v.crawford@gmail.com>
2017-03-16 21:35:50 -0700

    Replace anchors with router links

--- components/headline.jsx
+++ components/headline.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 export default ({className, children}) => {
   const cssClass = className === undefined ? '' : className
 
   return <header className={"headline " + cssClass}>
     <h1>
-      Hi, I'm <a className="url fn" href="/" rel="home author" title="my site">
+      Hi, I'm <Link className="url fn" to="/" rel="home author" title="my site">
         Britt Crawford
-      </a>
+      </Link>
     </h1>
   </header>
 }
--- pages/reading/index.jsx
+++ pages/reading/index.jsx
@@ -7,11 +7,12 @@ import 'css/gutenberg/src/style/gutenberg.scss'
 import { Pages } from 'lib/sort_utils'
 import { config } from 'config'
 import PageUtils from 'lib/page_utils'
+import {Link} from 'react-router'
 
 const WeekLink = ({page}) => {
   return (
     <li>
-      <a href={page.path}>{page.data.week}</a>&nbsp;
+      <Link to={page.path}>{page.data.week}</Link>&nbsp;
       &mdash; {page.data.articles.length} articles
     </li>
   )
@@ -28,9 +29,9 @@ export default ({route}) => {
           What am I reading?
         </h2>
         <p>
-          A short description of articles I have read and liked each week. Some of 
+          A short description of articles I have read and liked each week. Some of
           the articles I found insightful, those might have notes, others just
-          had interesting information, or a cool fact. 
+          had interesting information, or a cool fact.
         </p>
         <ul>
           {links}
