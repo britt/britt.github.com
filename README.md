d6dfbb5460561a0a939bb270cca6e9dd97445cd3
Britt Crawford <britt.v.crawford@gmail.com>
2017-03-16 21:27:40 -0700

    Use Link. Update 404 page

--- README.md
+++ README.md
@@ -14,10 +14,7 @@ This is my personal website. Right now it just has a business card-like list of 
 
 ### Todo
 
-- [] Blog
-- [] Update to use new Sheetsu API
 - [] fetch_weekly_reading can handle empty folders
-- [] Drafts
 - [] Fix error
 
 ```
@@ -29,30 +26,6 @@ TypeError: /EEXIST/.match is not a function
     at FS.mkdir (/Users/bcrawford/workspace/brittcrawford.com/bin/fetch_weekly_reading:64:28)
     at FSReqWrap.oncomplete (fs.js:117:15)
 ```
-
-#### Done
-- [-] Configure LINTer for ES6
-- [x] Change icon color
-- [x] Fix layout on phone
-- [x] Weekly reading notes
-  + [x] binary to fetch weekly reading notes as JSON
-  + [x] editor interface to update notes
-  + [x] re-order columns
-- [x] Deployment script
-  + [x] Build script
-  + [x] GitHub pages deployment
-- [x] Fix site title for weekly reading pages
-- [x] Hide last name in header on mobile
-- [x] RSS feed
-  + [x] Add all posts to feed (remove dep on path)
-  + [x] Date all posts
-- [x] Add Gutenberg shout out https://matejlatin.github.io/Gutenberg/
-- [x] Show assets in dev view
-- [x] Better page titles
-- [x] Extract Headline component
-- [x] Upgrade to gatsby 0.10.0
-- [x] Publish static pages rather than single page app
-- [x] publishing on a schedule
 
 
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Headline from 'components/headline'
 import Icon from 'components/icon'
 import 'css/gutenberg/src/style/gutenberg.scss'
+import {Link} from 'react-router'
 
 export default ({route}) => {
   return (
@@ -19,38 +20,38 @@ export default ({route}) => {
           </li>
           <li>
             <a href="https://github.com/britt" title="GitHub" rel="me"><i className="fa fa-github-alt fa-fw"></i><span>https://github.com/britt</span></a>
-          </li> 
+          </li>
           <li>
             <a href="http://www.linkedin.com/in/brittcrawford/" title="My LinkedIn Profile" rel="me"><i className="fa fa-linkedin fa-fw"></i><span>https://www.linkedin.com/in/brittcrawford/</span></a>
-          </li> 
+          </li>
           <li>
             <a href="http://illtemper.org" title="A stream of consciousness link blog"><i className="fa fa-link fa-fw"></i><span>http://illtemper.org</span></a>
-          </li> 
+          </li>
           <li>
             <a href="bitcoin:17nHbStzmWgcBEBSgVFT94Eo5hoq9h7Hh1" title="Bitcoin"><i className="fa fa-bitcoin fa-fw"></i><span>Bitcoin</span></a>
-          </li> 
+          </li>
           <li>
             <a href="/britt.gpg" title="PGP/GPG Public Key"><i className="fa fa-lock fa-fw"></i><span>PGP/GPG Key</span></a> &mdash;&nbsp;
-            <a href="https://keybase.io/britt" title="Keybase"><span>Keybase</span></a> &mdash;&nbsp; 
+            <a href="https://keybase.io/britt" title="Keybase"><span>Keybase</span></a> &mdash;&nbsp;
             <a href="http://pgp.mit.edu/pks/lookup?op=vindex&search=0x7C887E4EE58C84B3" title="MIT PGP Public Key Server"><span>MIT</span></a>
-          </li>                                                       
+          </li>
         </ul>
       </section>
       <section className="list-of-links">
         <h3>Projects</h3>
         <ul>
           <li>
-            <a href="/reading/">
+            <Link to="/reading/">
               What am I reading?
-            </a>
+            </Link>
             <summary>
               A weekly summary of articles that I have read and liked.
             </summary>
           </li>
           <li>
-            <a href="/cocktails/">
+            <Link to="/cocktails/">
               Cocktail Recipes
-            </a>
+            </Link>
             <summary>
               A small collection of cocktails that I've created.
             </summary>
@@ -60,12 +61,12 @@ export default ({route}) => {
               This site
             </a>
             <summary>
-              The source code for <a href="/">brittcrawford.com</a>. Look in 
-              the <a href="https://github.com/britt/britt.github.com">master branch</a> for 
-              the deployed code, or in 
-              the <a href="https://github.com/britt/britt.github.com/tree/gatsby">gatsby branch</a> to 
-              see the JavaScript and React that actually builds the static site. 
-              This site was built 
+              The source code for <Link to="/">brittcrawford.com</Link>. Look in
+              the <a href="https://github.com/britt/britt.github.com">master branch</a> for
+              the deployed code, or in
+              the <a href="https://github.com/britt/britt.github.com/tree/gatsby">gatsby branch</a> to
+              see the JavaScript and React that actually builds the static site.
+              This site was built
               with <a href="https://github.com/gatsbyjs/gatsby">Gatsby</a> and <a href="https://matejlatin.github.io/Gutenberg/">Gutenberg</a>.
             </summary>
           </li>
