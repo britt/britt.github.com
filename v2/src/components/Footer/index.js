3411184ad6ff11c4cff2d9ec54a53c919f655dcf
Britt Crawford <britt.v.crawford@gmail.com>
2018-02-05 14:05:08 -0800

    fix footer

--- v2/src/components/Footer/index.js
+++ v2/src/components/Footer/index.js
@@ -1,21 +1,18 @@
 import React from 'react'
 
 export default () => {
-  const license = `<div>
-      <div class="text-muted">
-        <p>
-          <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">
+  return <footer className='container page-footer'>
+    <div className='text-muted'>
+      <p>
+        <span xmlns='dct' dct='http://purl.org/dc/terms/' property='dct:title'>
             brittcrawford.com
           </span>
-          <span class="hidden" xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">
-            <a href="https://github.com/britt">britt</a>
-          </span> is licensed under a
-          <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">
+        <span className='hidden' xmlns='cc' cc='http://creativecommons.org/ns#' property='cc:attributionName'>
+          <a href='https://github.com/britt'>britt</a>
+        </span> is licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/3.0/deed.en_US'>
             Creative Commons Attribution-ShareAlike 3.0 Unported License
           </a>
-        </p>
-      </div>
-    </div>`
-
-  return <footer className='container page-footer' dangerouslySetInnerHTML={{ __html: license }} />
+      </p>
+    </div>
+  </footer>
 }
