d2dc02017c530c5a09ad532c295aabdb5fccdb86
Britt Crawford <britt.v.crawford@gmail.com>
2018-02-06 14:26:35 -0800

    change footer style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default () => {
-  return <footer className='container page-footer'>
+  return <footer className='container'>
     <p>
       <span> brittcrawford.com </span> is licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/3.0/deed.en_US'>
           Creative Commons Attribution-ShareAlike 3.0 Unported License
