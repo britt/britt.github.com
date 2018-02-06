1b7daf81b384a4bafec23cadc86fc30702b327dd
Britt Crawford <britt.v.crawford@gmail.com>
2018-02-05 22:39:19 -0800

    fix footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 export default () => {
   return <footer className='container page-footer'>
-    <div className='text-muted'>
-      <p>
-        <span> brittcrawford.com </span> is licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/3.0/deed.en_US'>
+    <p>
+      <span> brittcrawford.com </span> is licensed under a <a rel='license' href='http://creativecommons.org/licenses/by-sa/3.0/deed.en_US'>
           Creative Commons Attribution-ShareAlike 3.0 Unported License
         </a>
-      </p>
-    </div>
+    </p>
   </footer>
 }
